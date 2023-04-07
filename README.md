94cc8119cd3ebe0ac25c8b9cec1a78305ae67c98
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-04-08 01:19:58 +0700

    Let's go

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tam Dao 2023 (Kỷ niệm Tam Đảo 2023)
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/dddf8f76-81f8-44b1-acc3-3e5c39fd6afd/deploy-status)](https://app.netlify.com/sites/tamdao2023/deploys)
+
+This site built with [Next.js](https://nextjs.org), [Tailwind CSS](https://tailwindcss.com) and [Netlify](https://www.netlify.com).
+
+## Running Locally
+
+This application requires Node.js v18+.
+
+```sh
+git clone https://github.com/Retro90s/tamdao2023
+cd tamdao2023
+pnpm install
+pnpm dev
+```
---
+++ app/layout.tsx
@@ -0,0 +1,52 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Goodbye K39C | Microlens",
+  description: "Tiệc chia tay y đa khoa K39C HPMU",
+  openGraph: {
+    title: "Goodbye K39C | Microlens",
+    description: "Tiệc chia tay y đa khoa K39C HPMU",
+    url: "https://goodbyek39c.netlify.app",
+    siteName: "Goodbye K39C | Microlens",
+    images: [
+      {
+        url: "https://goodbyek39c.netlify.app/og.jpg",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "vi-VN",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Goodbye K39C | Microlens",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-black antialiased">{children}</body>
+    </html>
+  );
+}
