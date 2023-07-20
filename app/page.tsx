30696f9253afee32190270c77f31905291d7a8a6
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-20 16:17:20 +0700

    Final version

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import galleryIcon from '@/components/Icon';
+import Icon from '@/components/Icon';
 import Gallery from '@/components/Gallery';
 
 const title = 'Tạm Biệt K39C HPMU';
@@ -14,7 +14,7 @@ export default function Home() {
           <div className='after:content shadow-highlight after:shadow-highlight relative col-span-1 row-span-3 flex flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white after:pointer-events-none after:absolute after:inset-0 after:rounded-lg sm:col-span-2 lg:col-span-1 lg:row-span-2 lg:pt-0'>
             <div className='absolute inset-0 flex items-center justify-center opacity-20'>
               <span className='flex max-h-full max-w-full items-center justify-center'>
-                <galleryIcon />
+                <Icon />
               </span>
               <span className='absolute inset-x-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black'></span>
             </div>
--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,4 +1,4 @@
-export default function galleryIcon() {
+export default function Icon() {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
